60379dd8bdae0d05c639bb6788edbcc5b190cf98
superhakar <parithimalan.a@gmail.com>
2022-05-01 22:55:59 +0530

    Change Password Integrated

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -1,16 +1,32 @@
-import React,{useState} from "react";
-import {
-  Grid,
-  Paper,
-  Avatar,
-  TextField,
-  Button,
-} from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid, Paper, Avatar, TextField, Button } from "@material-ui/core";
 import KeyIcon from "@mui/icons-material/Key";
+import { notifySuccess, notifyError } from "../actionCreators/NotifyActions";
+import axios from "../common/AxiosConfig";
+import { useDispatch } from "react-redux";
 
 export const ChangePassword = () => {
   const avatarStyle = { backgroundColor: "#363062" };
+  const dispatch = useDispatch();
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = () => {
+    console.log("Change Password!!!!!!");
+    if (password === confirmPassword) {
+      axios
+        .post("/users/modify", { password })
+        .then((res) => {
+          console.log(res);
+          dispatch(notifySuccess("Password Changed Successfully"));
+        })
+        .catch((err) => {
+          dispatch(notifyError(err.response.data.errors[0].msg));
+        });
+    }
+    else{
+      dispatch(notifyError("Password must match"));
+    }
+  };
   return (
     <Grid className="page">
       <Paper
@@ -34,6 +50,8 @@ export const ChangePassword = () => {
           placeholder="Enter New password"
           type="password"
           fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           required
           style={{ marginBottom: "10px" }}
         />
@@ -43,6 +61,8 @@ export const ChangePassword = () => {
           type="password"
           fullWidth
           required
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
           style={{ marginBottom: "20px" }}
         />
         <Button
@@ -54,6 +74,7 @@ export const ChangePassword = () => {
             backgroundColor: "#363062",
             color: "#E9D5DA",
           }}
+          onClick={handleSubmit}
         >
           Change Password
         </Button>
