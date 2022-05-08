5ebef8f3f96a5275252b065eceaecf82d8921f6c
superhakar <parithimalan.a@gmail.com>
2022-05-08 19:19:02 +0530

    env variable added

--- src/common/AxiosConfig.js
+++ src/common/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:15000/",
+  baseURL: process.env.BACKEND_URL || "http://localhost:15000/",
 });
 
 instance.interceptors.request.use(
